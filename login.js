2d65afb32e2d68dc8ce412468fe2b88d1f5af17d
lokendra0905 <bannalokendrapal2003@gmail.com>
2022-11-10 21:39:41 +0530

    Day-3 tasks

---
+++ login.js
@@ -0,0 +1,13 @@
+document.querySelector("#submit").addEventListener("click", details)
+    function details(event) {
+        event.preventDefault()
+        let name = document.querySelector("#name").value
+        let email = document.querySelector("#email").value
+        let password = document.querySelector("#password").value
+
+        if(name == "Lokendra" && email == "lokendra1234@gmail.com" && password == 12345678){
+            alert("login Succesfully")
+        } else{
+            alert("Invalid Details")
+        }
+    }
